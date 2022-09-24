7fe96c156c5a49036bc0df1bae18d319cc0e1e2c
Santiago Lopez <santiagolopezpardo@gmail.com>
2022-09-23 22:07:12 -0500

    Initial commit

---
+++ src/main/java/com/fundamentals/spring/fundamentalsJavaSpringBoot/FundamentalsJavaSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.fundamentals.spring.fundamentalsJavaSpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FundamentalsJavaSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FundamentalsJavaSpringBootApplication.class, args);
+	}
+
+}
