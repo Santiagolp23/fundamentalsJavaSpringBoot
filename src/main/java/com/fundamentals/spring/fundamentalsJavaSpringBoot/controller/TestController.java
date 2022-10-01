17e5c3f9c3b63909fa9589c7dfa8e697374b22b3
Santiago Lopez <santiagolopezpardo@gmail.com>
2022-09-30 22:57:30 -0500

    Creating my own controller

---
+++ src/main/java/com/fundamentals/spring/fundamentalsJavaSpringBoot/controller/TestController.java
@@ -0,0 +1,17 @@
+package com.fundamentals.spring.fundamentalsJavaSpringBoot.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class TestController {
+    @RequestMapping
+    @ResponseBody
+    public ResponseEntity<String> functions() {
+        return new ResponseEntity<>("Hello from controller", HttpStatus.OK);
+    }
+
+}
