8a91314e8d8aa0ceb7c699e122efeb748865ad2a
Santiago Lopez <santiagolopezpardo@gmail.com>
2022-10-07 22:32:46 -0500

    using bean configuration to define dataSource

--- src/main/java/com/fundamentals/spring/fundamentalsJavaSpringBoot/configuration/MyBeanConfiguration.java
+++ src/main/java/com/fundamentals/spring/fundamentalsJavaSpringBoot/configuration/MyBeanConfiguration.java
@@ -1,8 +1,11 @@
 package com.fundamentals.spring.fundamentalsJavaSpringBoot.configuration;
 
 import com.fundamentals.spring.fundamentalsJavaSpringBoot.bean.*;
+import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
 
 @Configuration()
 public class MyBeanConfiguration {
@@ -24,6 +27,16 @@ public class MyBeanConfiguration {
     @Bean
     public MyBeanWithProperties beanProp(GeneralConfiguration generalConfiguration) {
         return new MyBeanWithPropertiesImpl(generalConfiguration);
+    }
+
+    @Bean
+    public DataSource dataSource() {
+        DataSourceBuilder dataSourceBuilder = DataSourceBuilder.create();
+        dataSourceBuilder.driverClassName("org.h2.Driver");
+        dataSourceBuilder.url("jdbc:h2:mem:testdb");
+        dataSourceBuilder.username("SA");
+        dataSourceBuilder.password("");
+        return dataSourceBuilder.build();
     }
 
 
