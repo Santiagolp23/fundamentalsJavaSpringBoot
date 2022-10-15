a32857f4eb6036929978c70b5197d677cb4b1cda
Santiago Lopez <santiagolopezpardo@gmail.com>
2022-10-14 22:55:12 -0500

    using @Transactional and showing how rollback works

--- src/main/java/com/fundamentals/spring/fundamentalsJavaSpringBoot/FundamentalsJavaSpringBootApplication.java
+++ src/main/java/com/fundamentals/spring/fundamentalsJavaSpringBoot/FundamentalsJavaSpringBootApplication.java
@@ -7,6 +7,7 @@ import com.fundamentals.spring.fundamentalsJavaSpringBoot.component.ComponentDep
 import com.fundamentals.spring.fundamentalsJavaSpringBoot.entity.User;
 import com.fundamentals.spring.fundamentalsJavaSpringBoot.pojo.UserPojo;
 import com.fundamentals.spring.fundamentalsJavaSpringBoot.repository.UserRepository;
+import com.fundamentals.spring.fundamentalsJavaSpringBoot.service.UserService;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -30,15 +31,18 @@ public class FundamentalsJavaSpringBootApplication implements CommandLineRunner 
     private MyBeanWithProperties myBeanWithProperties;
     private UserPojo userPojo;
     private UserRepository userRepository;
+    private UserService userService;
+
 
     @Autowired
-    public FundamentalsJavaSpringBootApplication(@Qualifier("componentTwoImplement") ComponentDependency componentDependency, MyBean myBean, MyBeanWithDependency myBeanWithDependency, MyBeanWithProperties myBeanWithProperties, UserPojo userPojo, UserRepository userRepository) {
+    public FundamentalsJavaSpringBootApplication(@Qualifier("componentTwoImplement") ComponentDependency componentDependency, MyBean myBean, MyBeanWithDependency myBeanWithDependency, MyBeanWithProperties myBeanWithProperties, UserPojo userPojo, UserRepository userRepository, UserService userService) {
         this.componentDependency = componentDependency;
         this.myBean = myBean;
         this.myBeanWithDependency = myBeanWithDependency;
         this.myBeanWithProperties = myBeanWithProperties;
         this.userPojo = userPojo;
         this.userRepository = userRepository;
+        this.userService = userService;
     }
 
     public static void main(String[] args) {
@@ -48,7 +52,7 @@ public class FundamentalsJavaSpringBootApplication implements CommandLineRunner 
     @Override
     public void run(String... args) {
         saveUserInDataBase();
-        LOGGER.info("User gotten by getAllByBirthDateAndEmail method: " + userRepository.getAllByBirthDateAndEmail(LocalDate.of(2021, 3, 13), "john@domain.com").orElseThrow(() -> new RuntimeException("User couldn't be found")));
+        saveWithErrorTransactional();
     }
 
     private void saveUserInDataBase() {
@@ -64,6 +68,29 @@ public class FundamentalsJavaSpringBootApplication implements CommandLineRunner 
         User user10 = new User("Paola", "paola2@domain.com", LocalDate.of(2021, 4, 10));
         List<User> list = Arrays.asList(user1, user2, user3, user4, user5, user6, user7, user8, user9, user10);
         userRepository.saveAll(list);
+    }
+
+    private void saveWithErrorTransactional() {
+        User test1 = new User("TestTransactional1", "TestTransactional1@domain.com", LocalDate.now());
+        User test2 = new User("TestTransactional2", "TestTransactional2@domain.com", LocalDate.now());
+        User test3 = new User("TestTransactional3", "TestTransactional1@domain.com", LocalDate.now());
+        User test4 = new User("TestTransactional4", "TestTransactional4@domain.com", LocalDate.now());
+
+        List<User> users = Arrays.asList(test1, test2, test3, test4);
+
+        try {
+            userService.saveTransactional(users);
+
+        } catch (Exception e) {LOGGER.error("this in an exception inside the transactional method: " + e);}
+
+
+
+        userService.getAllUsers().forEach(user -> LOGGER.info("this is the user inside the transactional method: " + user));
+
+
+
+
+
     }
 
     public void getInformationJpql() {
@@ -83,6 +110,7 @@ public class FundamentalsJavaSpringBootApplication implements CommandLineRunner 
         getInformationJpql();
         findByNameSorted();
         userRepository.findByNameContainingAndBirthDateBetween("i", LocalDate.of(2021, 6, 1), LocalDate.of(2021, 9, 30)).forEach(u -> LOGGER.info(("user findByNameContainingAndBirthDateBetween: " + u)));
+        LOGGER.info("User gotten by getAllByBirthDateAndEmail method: " + userRepository.getAllByBirthDateAndEmail(LocalDate.of(2021, 3, 13), "john@domain.com").orElseThrow(() -> new RuntimeException("User couldn't be found")));
 
         try {
             int value = 10 / 0;
